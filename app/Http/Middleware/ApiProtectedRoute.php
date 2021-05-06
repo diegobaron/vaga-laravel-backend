da8c489e2c35a23265e75978c807e153f50e1f30
Diego Baron <diegobaron_bjo@hotmail.com>
2021-05-06 19:34:03 -0300

    configurações autenticação

---
+++ app/Http/Middleware/ApiProtectedRoute.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+use Closure;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ApiProtectedRoute extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch(\Exception $e) {
+            if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token is Invalid'], Response::HTTP_UNAUTHORIZED);
+            } else if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token is Expired'], Response::HTTP_UNAUTHORIZED);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found'], Response::HTTP_UNAUTHORIZED);
+            }
+        }
+        return $next($request);
+    }
+}

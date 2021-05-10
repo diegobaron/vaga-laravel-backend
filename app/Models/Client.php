d52c87474ce25a10bcf036befd5a8da75ddd309a
Diego Baron <diegobaron_bjo@hotmail.com>
2021-05-09 22:42:49 -0300

    criado model Order e configurado model client e product

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "clients";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+    ];
 }

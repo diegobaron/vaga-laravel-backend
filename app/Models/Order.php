20de716ae3712718025f4b2c5a2749a86a8737ff
Diego Baron <diegobaron_bjo@hotmail.com>
2021-05-11 18:12:10 -0300

    configuração model Order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "orders";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'client_id',
+        'status',
+        'amount'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'client_id' => 'integer',
+    ];
 }
